3a139638006cdcd66573e4cc394206fd4eaf9a10
Sean <Sean.Donahue.5311@gmail.com>
2020-09-10 15:07:14 -0400

    Initial commits of python package for common cross-dataset tasks

---
+++ setup.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+from setuptools import setup, find_packages
+
+setup(
+    name='cross-dataset-common',
+    version='0.1',
+    description='Utilities for preparing processed data to be loaded into database',
+    url='https://github.com/hubmapconsortium/cross-modality-common',
+    author='Sean Donahue',
+    author_email='seandona@andrew.cmu.edu',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    packages=find_packages(),
+    install_requires=[
+        'anndata>=0.7.3',
+        'requests>=2.22.0',
+        'pyyaml>=5.3',
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'get_tissue_type=cross-dataset-common.cross_dataset_tools:get_tissue_type',
+            'get_gene_dicts=cross-dataset-common.cross_dataset_tools:get_gene_dicts',
+            'get_rows=cross-dataset-common.cross_dataset_tools:get_rows',
+        ],
+    },
+)
