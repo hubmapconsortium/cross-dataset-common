83a636680d8b9f92326075a61d652f745beaae30
Sean <Sean.Donahue.5311@gmail.com>
2020-11-25 11:53:54 -0500

    Updates to code for creating minimal datasets

--- cross_dataset_common/__init__.py
+++ cross_dataset_common/__init__.py
@@ -241,38 +241,21 @@ def make_mini_cell_df(file):
 
     new_file = "mini_" + file.stem + ".hdf5"
     with pd.HDFStore(new_file) as store:
-        if file.stem == "codex":
-            store.put("cell", mini_cell_df)
-        else:
-            store.put("cell", mini_cell_df, format="t")
-
+        store.put("cell", mini_cell_df)
     return cell_ids
 
 
 def make_mini_quant_df(file, cell_ids):
 
-    with pd.HDFStore(file) as store:
-        genes = list(store.get("quant").columns)[:1000]
-        chunks = len(store.get("quant").index) // 1000 + 1
-        filtered_chunks = []
+    csv_file = file.stem + '.csv'
+    quant_df = pd.read_csv(csv_file)
+    genes = list(quant_df['gene_id'].unique())[:1000]
+    quant_df.set_index('gene_id', inplace=True, drop=False)
+    quant_df = quant_df[genes]
+    quant_df.set_index('cell_id', inplace=True, drop=False)
+    quant_df = quant_df[cell_ids]
 
-        for i in range(chunks):
-            print("Loading chunk " + str(i) + " out of " + str(chunks))
-            chunk = store.select("quant", start=i * 1000, stop=(i + 1) * 1000)
-            filtered_chunk = chunk[genes]
-            empty = True
-            print(cell_ids[0])
-            for i in chunk.index:
-                if i in cell_ids:
-                    empty = False
-            if not empty:
-                filtered_chunk = filtered_chunk[cell_ids]
-                filtered_chunks.append(filtered_chunk)
-
-    filtered_quant_df = pd.concat(filtered_chunks)
-    new_file = "mini_" + file.stem + ".hdf5"
-    with pd.HDFStore(new_file) as store:
-        store.put("quant", filtered_quant_df)
+    quant_df.to_csv('mini_' + csv_file)
 
     return genes
 
@@ -292,12 +275,11 @@ def make_mini_pval_dfs(file, gene_ids):
     return
 
 
-def create_minimal_dataset(hdf_files: List[Path]):
+def create_minimal_dataset(hdf_file: Path):
 
-    for file in hdf_files:
-        cell_ids = make_mini_cell_df(file)
-        if file.stem in ["atac", "rna"]:
-            gene_ids = make_mini_quant_df(file, cell_ids)
-            make_mini_pval_df(file, gene_ids)
+    cell_ids = make_mini_cell_df(file)
+    if hdf_file.stem in ["atac", "rna"]:
+        gene_ids = make_mini_quant_df(hdf_file, cell_ids)
+        make_mini_pval_dfs(hdf_file, gene_ids)
 
 
