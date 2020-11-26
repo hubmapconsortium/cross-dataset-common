59f49fc5a44e0570a8a790add494f117dd241b62
Sean <Sean.Donahue.5311@gmail.com>
2020-11-25 23:12:41 -0500

    Removing superfluous decoding

--- cross_dataset_common/__init__.py
+++ cross_dataset_common/__init__.py
@@ -12,7 +12,7 @@ import hashlib
 from scipy.sparse import coo_matrix
 
 def hash_cell_id(semantic_cell_ids: pd.Series):
-    hash_list = [hashlib.sha256(semantic_cell_id.encode('UTF-8')).hexdigest().decode('UTF-8') for semantic_cell_id in semantic_cell_ids]
+    hash_list = [hashlib.sha256(semantic_cell_id.encode('UTF-8')).hexdigest() for semantic_cell_id in semantic_cell_ids]
     return pd.Series(hash_list)
 
 def make_quant_df(adata: anndata.AnnData):
