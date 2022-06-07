81556bc796cfd2a89e97eca1b1207e838383efd0
Sean Donahue <seandonahue5311@gmail.com>
2022-06-07 10:46:22 -0400

    Factoring out code for making minimal adata

--- cross_dataset_common/__init__.py
+++ cross_dataset_common/__init__.py
@@ -520,3 +520,12 @@ def precompute_values_series(cell_df, adata):
         values_series_dict.update(vsd)
 
     return values_series_dict
+
+def make_minimal_adata(adata):
+
+    X = csr_matrix(adata.X)
+    obs = pd.DataFrame(index=adata.obs["cell_id"])
+    var = pd.DataFrame(index=adata.var.index)
+
+    min_adata = anndata.AnnData(X=X, obs=obs, var=var)
+    min_adata.write_h5ad("rna.h5ad")
